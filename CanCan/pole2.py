ab3ea86b0bb1e6d252f9ae8a7b3939af4a8a1502
dangerhotrod <danger@waste.org>
2017-01-03 21:29:20 -0600

    pole2 code update

--- CanCan/pole2.py
+++ CanCan/pole2.py
@@ -1,6 +1,67 @@
 #! /usr/bin/python3
-
+from Adafruit_I2C import Adafruit_I2C
+import RPi.GPIO as GPIO
+from pins import *
+from dotstar import Adafruit_DotStar
+import random
 import threading
+import sys
+import select
+import time
+import socket
+
+#Possible sensor addresses (suffix correspond to DIP switch positions)
+SENSOR_ADDR_OFF_OFF = 0x26
+SENSOR_ADDR_OFF_ON = 0x22
+SENSOR_ADDR_ON_OFF = 0x24
+SENSOR_ADDR_ON_ON = 0x20
+
+#Set the sensor address here
+sensorAddr = SENSOR_ADDR_ON_ON
+
+# Light stuff
+SLEEPING = 0
+TRIGGERED = 1
+UPDOWN = 2
+FLASHING = 3
+RAINBOW = 4
+FIREWORKS = 5
+FIREWORKS2 = 6
+
+state = SLEEPING
+count = 0
+lasttrigger = 0
+step = 2
+pos = 0
+color1 = 0
+color2 = 0
+cycles = 0
+endpos = 0
+startpos = 0
+colorlength = 75.0
+
+#read command line and set mode
+mode = FLASHING
+
+
+
+class sensor:
+    def __init__(self, addr, bus):
+        self.i2c = Adafruit_I2C(0x20, bus=1)
+        #starts sensor watching proximity
+        self.i2c.write8(0x3, 0xFE)
+        
+    def check(self):
+        val = self.i2c.readU8(0)
+        # the second bit is true, nothing is detected
+        if val & 0x2:
+            #print "Nothing"
+            return False
+            
+        else:
+            #print "Something"
+            return True
+
 
 # placeholder class to deal with the process that manages
 class LightStrip (threading.Thread):
@@ -11,30 +72,99 @@ class LightStrip (threading.Thread):
         self.counter = counter
     def run(self):
         print "Starting " + self.name
-        controlFunction(self.name, self.counter, 5)
         print "Exiting " + self.name
     def hole_in_one_pattern(self):
+        self.clear()
         print "\n()()()()()()()()()()()()\n"
         print "Make the hole in one lights go"
         print "\n()()()()()()()()()()()()\n"
         
-def controlFunction(threadName, delay, counter):
-    while counter:
-        if exitFlag:
-            threadName.exit()
-        time.sleep(delay)
-        print "%s: %s" % (threadName, time.ctime(time.time()))
-        counter -= 1
+        for x in range(numpixels):
+            strip.setPixelColor(x, 255)
+        strip.show()
+        time.sleep(1)
+        self.clear()
+    def ball_sensed(self):
+        self.clear()
+        print "\n()()()()()()()()()()()()\n"
+        print "I saw a ball"
+        print "\n()()()()()()()()()()()()\n"
+        pattern = random.randint(0, 1)
+        if pattern:
+            self.updown()
+        else:
+            self.flashing()
+        #for x in range(numpixels):
+        #    strip.setPixelColor(x, 30)
+        #strip.show()
+        time.sleep(1)
+        self.clear()
+    def clear(self):
+        color = 0
+        for x in range(numpixels):
+            strip.setPixelColor(x, color)
+        strip.show()
+    def updown(self):
+        print "\n()()()()()()()()()()()()\n"
+        print "UPDOWN"
+        print "\n()()()()()()()()()()()()\n"
+        dir = 1
+        count = 0
+        pos = random.randint(0, 255)
+        colorspeed = colorlength/numpixels
+        for x in range(numpixels):
+            time.sleep(1.0/150.0)
+            count += dir
+            pos += dir * colorspeed
+            color = colorWheel(int(pos) % 255)
+            strip.setPixelColor(count, color)
+            strip.show()
+        dir = -1
+        for x in reversed(range(numpixels)):
+            time.sleep(1.0/150.0)
+            count += dir
+            pos += dir * colorspeed
+            color = colorWheel(int(pos) % 255)
+            strip.setPixelColor(count+1, 0)
+            strip.setPixelColor(count, color)
+            strip.show()
+            
+    def flashing(self):
+        print "\n()()()()()()()()()()()()\n"
+        print "FLASHING"
+        print "\n()()()()()()()()()()()()\n"
+        pos = random.randint(0,255)
+        color1 = colorWheel(int(pos) % 255)
+        print "color1 is %d" % color1
+        pos2 = random.randint(0,255)
+        color2 = colorWheel(int(pos2) % 255)
+        print "color2 is %d" % color2
+        for z in range(1, 8):
+            for x in range(numpixels):
+                strip.setPixelColor(x, color1)
+            strip.show()
+            time.sleep(1.0/5)
+            for y in range(numpixels):
+                strip.setPixelColor(y, color2)
+            strip.show()
+            time.sleep(1.0/5)
+        
+    
+#            strip.setPixelColor(count + 1, 0)
+#            if count < 0:
+#                clear()
+#                state = SLEEPING
+#                
+#            if count == numpixels:
+#                dir = -1
+
 
 
 """Check for input every 0.1 seconds. Treat available input
 immediately, but do something else if idle."""
 
 
-import sys
-import select
-import time
-import socket
+mysensor = sensor(0x20, 1)
 
 program_id = 2
 host = 'localhost'
@@ -90,7 +220,11 @@ def idle_work():
   global s
   global pole
   now = time.time()
-  message_to_send = str(program_id) + "=" + "idle0"
+  currenttrigger = mysensor.check()
+  if currenttrigger:
+    message_to_send = str(program_id) + "=" + "ball0"
+    pole.ball_sensed()
+    s.send(message_to_send)
   #s.send(message_to_send)
   try:
     data = s.recv(size)
